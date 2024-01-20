22c206fc80ffcfb659288a267c78932cdec8e201
Salman Lazar <Salman.Lazar@signature.co.za>
2024-01-20 21:19:08 +0200

    Added Background Services Example

---
+++ BackgroundServices/HostedServices.cs
@@ -0,0 +1,14 @@
+﻿public class HostedServices 
+{
+    //What is a hosted service?
+    //A hosted service is a class with background task logic that
+    //implements the IHostedService interface.
+
+    //What is the IHostedService interface?
+    //The IHostedService interface is used to notify your application
+
+    //when the app host has started and stopped.
+    //The IHostedService interface has two methods:
+    //StartAsync and StopAsync.
+
+}
---
+++ BackgroundServices/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddHostedService<MyBackgroundService>();
+            });
+}
+
+public class MyBackgroundService : IHostedService, IDisposable
+{
+    private Timer? _timer;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Background service is starting.");
+
+
+        _timer = new Timer(DoWork, null, TimeSpan.Zero, // Start immediately
+            TimeSpan.FromSeconds(5)); // Run every 5 seconds
+
+        return Task.CompletedTask;
+    }
+
+    private void DoWork(object? state)
+    {
+        Console.WriteLine($"Background service is doing its work. {DateTime.Now}");
+        // Do some work here that runs every 5 seconds ...
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        Console.WriteLine("Background service is stopping.");
+
+        _timer?.Change(Timeout.Infinite, 0);
+
+        return Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        _timer?.Dispose();
+    }
+}
