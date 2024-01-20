7da8f6dee5220162ff2f6c7c53974df9db48f9bc
Salman Lazar <Salman.Lazar@signature.co.za>
2024-01-20 21:31:07 +0200

    Added Result Pattern Example Instead of throwinng exceptions which are expensive.

---
+++ ResultPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using CSharpFunctionalExtensions;
+
+static void Main(string[] args)
+{
+    var result = DoWork("Hello");
+    string? message = result.IsSuccess ? $"Success: {result.Value}" : $"Failed: {result.Error}";
+    Console.WriteLine(message);
+
+    result = DoWork(null);
+    message = result.IsSuccess ? $"Success: {result.Value}" : $"Failed: {result.Error}";
+    Console.WriteLine(message);
+}
+
+static Result<string> DoWork(string input)
+{
+    if (string.IsNullOrEmpty(input))
+        return Result.Failure<string>("Input cannot be null or empty");
+
+    string output = input + " World";
+
+    return Result.Success(output);
+}
+
+
+
+
+
+
