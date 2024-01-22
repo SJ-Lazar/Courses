700a0c8d3b7c9940771970446547a10d39e3586b
Salman Lazar <Salman.Lazar@signature.co.za>
2024-01-22 10:07:05 +0200

    Added Nunit Tests

---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("357f3fe1-53ac-4ccf-b7f2-2b0659a80b71")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        public void CanBeCancelledBy_UserIsAdmin_ReturnsTrue() => 
+            Assert.IsTrue(new Reservation().CanBeCancelledBy(new User { IsAdmin = true }));
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUserCancellingTheReservation_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation() { MadeBy = user};
+            Assert.IsTrue(reservation.CanBeCancelledBy(user));
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_AnotherUserCancellingTheReservation_ReturnsFalse()
+        {
+            var reservation = new Reservation() { MadeBy = new User()};
+            Assert.IsFalse(reservation.CanBeCancelledBy(new User()));
+        }
+    }
+}
