1550b4e38c553b5f1214191e439ad6c0f7972edd
Salman Lazar <Salman.Lazar@signature.co.za>
2024-01-23 23:28:47 +0200

    Added a machine learning demo

--- source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
+++ source-code-starter+(1)/source-code-starter/TestNinja/TestNinja.UnitTests/MathTests.cs
@@ -4,28 +4,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
     [TestFixture]
     public class MathTests
     {
+        private Fundamentals.Math _math;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
+
         [Test]
+        [Ignore("Because I wanted to!")]
         public void Add_WhenCalled_ReturnTheSumOfArguments() 
-            => Assert.That(new Fundamentals.Math().Add(1, 2), Is.EqualTo(3));
+            => Assert.That(_math.Add(1, 2), Is.EqualTo(3));
+
+        //[Test]
+        //public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument() 
+        //    => Assert.That(_math.Max(2, 1), Is.EqualTo(2));
+
+        //[Test]
+        //public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument() 
+        //    => Assert.That(_math.Max(1, 2), Is.EqualTo(2));
+
+        //[Test]
+        //public void Max_ArgumentsAreEqual_ReturnTheSameArgument() 
+        //    => Assert.That(_math.Max(1, 1), Is.EqualTo(1));
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument() 
-            => Assert.That(new Fundamentals.Math().Max(2, 1), Is.EqualTo(2));
-
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument() 
-            => Assert.That(new Fundamentals.Math().Max(1, 2), Is.EqualTo(2));
-
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnTheSameArgument() 
-            => Assert.That(new Fundamentals.Math().Max(1, 1), Is.EqualTo(1));
-
-
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a , int b, int expectedResult) 
+            => Assert.That(_math.Max(a, b), Is.EqualTo(expectedResult));
     }
 }
